bbf8b6fb10e7c40b4279630c6705c3a5f9728c79
Il'ya Protasevich <105548105+Protasevich-i@users.noreply.github.com>
2022-05-20 09:53:18 +0700

    Add files via upload

---
+++ Celsius to Fahrenheit converter/js/convert.js
@@ -0,0 +1,10 @@
+/* convert.js */
+
+/* project. Цельсий в Фаренгейты.
+Завершите функцию convertCelsiusToFahrenheit, чтобы она возвращала полученную температуру
+в градусах Цельсия, в градусах Фаренгейта. Формула для перевода
+следующая: (numberInCelsius * 9 / 5) + 32 */
+
+export function convertCelsiusToFahrenheit(celsius) {
+    return (celsius * 9 / 5) + 32
+}
